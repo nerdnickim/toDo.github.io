b676e25bbad2a627428213868c594ef69babf8a4
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-09-09 21:53:41 +0900

    mend

--- js/lastWorkJs/clock-gretting.js
+++ js/lastWorkJs/clock-gretting.js
@@ -1,54 +1,59 @@
 const wrapper = document.querySelector(".wrapper");
 
 const clockWrapper = wrapper.querySelector(".clock__wrapper"),
-    clock = clockWrapper.querySelector("h1");
+	clock = clockWrapper.querySelector("h1");
 
 const userWrapper = wrapper.querySelector(".user__js"),
-    userInput = userWrapper.querySelector("input"),
-    userShow = userWrapper.querySelector("h1");
+	userInput = userWrapper.querySelector("input"),
+	userShow = document.querySelector(".userName"),
+	showList = document.querySelector(".showList");
 
-    const USER_CURRENT = "user";
+const USER_CURRENT = "user";
 
-    const SHOWING = "showing"
+const SHOWING = "showing";
 
+function saveUser(text) {
+	localStorage.setItem(USER_CURRENT, text);
+}
 
-    function saveUser(text){
-        localStorage.setItem(USER_CURRENT, text);
-    }
+function userInputHandle(e) {
+	e.preventDefault();
+	const userName = e.target[0].value;
+	userInput.classList.remove(SHOWING);
+	userShow.innerText = `Hello!! ${userName}`;
+	showList.classList.remove("showList");
+	saveUser(userName);
+}
 
-    function userInputHandle(e){
-        e.preventDefault();
-        const userName = e.target[0].value;
-        userInput.classList.remove(SHOWING);
-        userShow.innerText = `Hello!! ${userName}`;
-        saveUser(userName);
-    }
+function currentUserHandle(text) {
+	userInput.classList.remove(SHOWING);
+	userShow.innerText = `Hello!! ${text}`;
+}
 
-    function currentUserHandle(text){
-        userInput.classList.remove(SHOWING);
-        userShow.innerText = `Hello!! ${text}`
-    }
+function clockInit() {
+	const date = new Date();
+	const hours = date.getHours();
+	const minutes = date.getMinutes();
+	const seconds = date.getSeconds();
 
-    function clockInit(){
-        const date = new Date();
-        const hours = date.getHours();
-        const minutes = date.getMinutes();
-        const seconds = date.getSeconds();
+	clock.innerText = `${hours < 10 ? `0${hours}` : `${hours}`} : ${
+		minutes < 10 ? `0${minutes}` : `${minutes}`
+	} : ${seconds < 10 ? `0${seconds}` : `${seconds}`}`;
+}
 
-        clock.innerText = `${hours < 10 ? `0${hours}` : `${hours}`} : ${minutes < 10 ? `0${minutes}` : `${minutes}`} : ${seconds < 10 ? `0${seconds}` : `${seconds}`}`
-    }
+if (clockWrapper) {
+	setInterval(clockInit, 1000);
+}
 
-    if(clockWrapper){
-        setInterval(clockInit, 1000)
-    }
+function loadLs() {
+	const currentUser = localStorage.getItem(USER_CURRENT);
+	if (currentUser === null) {
+		userWrapper.addEventListener("submit", userInputHandle);
+		showList.classList.add("showList");
+	} else {
+		currentUserHandle(currentUser);
+		showList.classList.remove("showList");
+	}
+}
 
-    function loadLs(){
-        const currentUser = localStorage.getItem(USER_CURRENT);
-        if(currentUser === null){
-            userWrapper.addEventListener("submit", userInputHandle);
-        } else {
-            currentUserHandle(currentUser);
-        }
-    }
-
-    loadLs();
+loadLs();
